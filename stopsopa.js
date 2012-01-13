b154c99859110b5c14b2fd94d5a20f7bd7245342
Estelle Weyl <estelle@weyl.org>
2012-01-12 18:27:25 -0800

    Script to push page off screen and add a div with STOP SOPA in the top left of your original page

---
+++ stopsopa.js
@@ -0,0 +1,21 @@
+var stopsopa = {
+	pagebody : document.getElementsByTagName('body')[0],
+	sopaText : "Stop SOPA", // change to whatever you want
+	date : new Date(),
+	blackout : function(){
+		stopsopa.pagebody.style.position = "absolute";
+		stopsopa.pagebody.style.left = "-2000px";
+		var div = document.createElement('div');
+		div.setAttribute('style', 'position:absolute; left: 2000px; top: 0; font-size: 100px; color: red; white-space: nowrap; font-family: arial, san-serif; font-weight: bold;');
+		var text = document.createTextNode(stopsopa.sopaText);
+		div.appendChild(text)
+		stopsopa.pagebody.appendChild(div);	
+	},
+	
+	actionDate: function(){
+		if(stopsopa.date.getMonth()	== 0 && stopsopa.date.getDate()	== 18 && stopsopa.date.getYear() == 112){
+			stopsopa.blackout();	
+		} 
+	}
+}
+stopsopa.actionDate();
